2323f21012e894e0c8361e86bdb5b5edb1170075
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2023-09-20 20:21:27 -0400

    Update registration logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'pages#home'
+  namespace :api do
+    namespace :v1 do
+      scope :users, module: :users do
+        post '/', to: 'registrations#create', as: :user_registration
+      end
+      resources :steps
+      resources :cards
+      resources :users
+    end
+  end
+  
+  scope :api do
+    scope :v1 do
+      use_doorkeeper do
+        skip_controllers :authorizations, :applications, :authorized_applications
+      end
+    end
+  end
 
-  use_doorkeeper
-  devise_for :users
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :steps
-  #     resources :cards
-  #     resources :users
-  #   end
-  # end
+  # devise_for :users
 
   draw :api
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
